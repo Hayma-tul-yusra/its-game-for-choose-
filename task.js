3996ebdbcc846951985c5d8f46f852f00b93767b
Hayma-tul-yusra <164422986+Hayma-tul-yusra@users.noreply.github.com>
2024-03-25 02:36:03 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const randNum = Math.floor(Math.random() * 10 + 1);
+const ans = await inquirer.prompt([{
+        name: "userGuessNum",
+        type: "number",
+        message: "please guess a number from 1 to 10",
+    }]);
+if (ans.userGuessNum === randNum) {
+    console.log("you win");
+}
+else {
+    console.log("you lose");
+}
